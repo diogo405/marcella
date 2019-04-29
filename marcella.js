bb91fbb8fda8c353df4ea7e88a1a2afcec8d2ffb
Diogo Goncalves <diogo@ticketmates.com.au>
2019-04-29 15:17:25 +1000

    Add support to form selector

--- marcella.js
+++ marcella.js
@@ -7,17 +7,19 @@
  */
 class Marcella {
 	
-	constructor() {
+	constructor(form) {
 		if (!this.checkSupport()) {
 			console.log('Marcella > local storage is not supported by this browser');
 			return;
 		}
+
+		this.form = form ? form : '';
 		this.selector = '.js-marcella';
 		this.loadData();
 		this.bindFields();
 	}
 
-	get fields() { return Array.from(document.querySelectorAll(this.selector)); }
+	get fields() { return Array.from(document.querySelectorAll(`${this.form} ${this.selector}`)); }
 
 	checkSupport() {
 		return window.localStorage;
@@ -28,7 +30,7 @@ class Marcella {
 			let key = field.name;
 			let savedValue = window.localStorage.getItem(key);
 			if (savedValue) {
-				document.querySelector(`*[name="${field.name}"]`).value = savedValue;
+				document.querySelector(`${this.form} *[name="${field.name}"]`).value = savedValue;
 			}
 		});
 	}
